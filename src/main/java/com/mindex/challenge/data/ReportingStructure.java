669ac65dae4f5b92e15d3abc6fb592d61d59babd
En Emerson <007emersone@gmail.com>
2023-06-18 22:31:07 -0500

    feat: add some boilerplate to ReportingStructure

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -2,4 +2,20 @@ package com.mindex.challenge.data;
 
 public class ReportingStructure {
 
+    private Employee employee;
+    private int numberOfReports;
+
+    public ReportingStructure() {
+
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+    public int getNumberOfReports() {
+        return numberOfReports;
+    }
 }
